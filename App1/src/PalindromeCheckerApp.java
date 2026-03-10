383b3f4679dc41c425e410050e577b2afd00e23a
Jaswanth291 <gaddamjaswanth550@gmail.com>
2026-03-10 14:24:50 +0530

    UC6: Implement palindrome check using Queue and Stack

--- App1/src/PalindromeCheckerApp.java
+++ App1/src/PalindromeCheckerApp.java
@@ -1,40 +1,41 @@
-import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
+import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
 
-        // Push characters into stack
+        // Add characters to stack and queue
         for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
+            char ch = input.charAt(i);
+            stack.push(ch);
+            queue.add(ch);
         }
 
         boolean isPalindrome = true;
 
-        // Compare with popped characters
-        for (int i = 0; i < input.length(); i++) {
-
-            char ch = stack.pop();
-
-            if (input.charAt(i) != ch) {
+        // Compare stack and queue
+        while (!stack.isEmpty()) {
+            if (stack.pop() != queue.remove()) {
                 isPalindrome = false;
                 break;
             }
         }
 
-        // Result
-        if (isPalindrome)
-            System.out.println("1Palindrome");
-        else
-            System.out.println("Not a Palindrome");
+        if (isPalindrome) {
+            System.out.println("The string is a Palindrome");
+        } else {
+            System.out.println("The string is NOT a Palindrome");
+        }
 
         sc.close();
     }
